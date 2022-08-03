4f736bfb2f5f616410a4cca9f1152ede873907c5
d1ng0ls <4b4c4t3666@gmail.com>
2022-08-03 11:54:28 -0300

    blog

--- core/conexao_mysql.php
+++ core/conexao_mysql.php
@@ -7,7 +7,7 @@
         $senha = '';
         $conexao = mysqli_connect($servidor, $usuario, $senha, $banco, $port);
 
-        if("$conexao"){
+        if(!$conexao){
             echo 'Erro: Não foi possível conectar ao MySql.' . PHP_EOL;
             echo 'Debugging errno: ' . mysqli_connect_errno() . PHP_EOL;
             echo 'Debugging error: ' . mysqli_connect_error() . PHP_EOL;
